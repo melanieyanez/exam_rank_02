58604ee91863baab4af266ab178a4d6417d474fd
Melanie Yanez pena <melanieyanez@gmail.com>
2023-04-24 17:02:33 +0200

    level 1 done sauf rot

---
+++ fizzbuzz.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myanez-p <myanez-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/13 15:25:31 by myanez-p          #+#    #+#             */
+/*   Updated: 2023/01/13 15:56:30 by myanez-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	write_nbr(int num)
+{
+	char	str[10] = "0123456789";
+
+	if (num >= 10)
+		write_nbr(num / 10);
+	write (1, &str[num % 10], 1);
+}
+
+int	main(void)
+{
+	int	num;
+
+	num = 1;
+
+	while (num <= 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (num % 3 == 0)
+			write(1, "fizz", 4);
+		else if (num % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			write_nbr(num);
+		write(1, "\n", 1);
+		num++;
+	}
+}
